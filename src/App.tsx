0c90390500a80305053238f0d575f9c69826bcb4
osbornetunde <osbornetunde@gmail.com>
2021-02-19 15:32:28 +0100

    completed the transpiling process

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,41 @@
 import * as esbuid from 'esbuild-wasm'
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 
 const App = () => {
+    const ref = useRef<any>(null);
     const  [input, setInput ] = useState('');
     const [code, setCode] = useState('');
 
     const startService = async () => {
-        const service = await esbuid.startService({
+       ref.current = await esbuid.startService({
             worker: true,
             wasmURL: '/esbuild.wasm'
         })
-        console.log(service);
+
     }
     useEffect(() =>{
         startService();
     },[])
-    const onClick= () => console.log(input)
-    return(<div>
-<textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
-
+    const onClick= async () => {
+        if(!ref.current){
+            return;
+        }
+        const result = await ref.current.transform(input, {
+            loader:'jsx',
+            target:'es2015'
+        })
+        setCode(result.code)
+    }
+    return(
         <div>
-            <button onClick={onClick}>Submit</button>
+            <textarea value={input} onChange={e => setInput(e.target.value)}/>
+                <div>
+                    <button onClick={onClick}>Submit</button>
+                </div>
+                <pre>{code}</pre>
         </div>
-        <pre>{code}</pre>
-    </div>)
+    )
 }
 
 export default App;
