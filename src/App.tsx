45be289239a3e80220c75c3f6f157a977a6577c3
osbornetunde <osbornetunde@gmail.com>
2021-02-19 15:18:52 +0100

    setup esbuild.wasm for transpiling and bundling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,30 @@
-import React from 'react';
+import * as esbuid from 'esbuild-wasm'
+import React, { useState, useEffect } from 'react';
 
 
-const App = () => <div>Hello</div>
+const App = () => {
+    const  [input, setInput ] = useState('');
+    const [code, setCode] = useState('');
+
+    const startService = async () => {
+        const service = await esbuid.startService({
+            worker: true,
+            wasmURL: '/esbuild.wasm'
+        })
+        console.log(service);
+    }
+    useEffect(() =>{
+        startService();
+    },[])
+    const onClick= () => console.log(input)
+    return(<div>
+<textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
+
+        <div>
+            <button onClick={onClick}>Submit</button>
+        </div>
+        <pre>{code}</pre>
+    </div>)
+}
 
 export default App;
