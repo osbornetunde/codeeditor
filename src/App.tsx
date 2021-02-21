2c9f433a38d73789c4cf1bc19ed8d3fbaed9eb5a
osbornetunde <osbornetunde@gmail.com>
2021-02-21 23:59:47 +0100

    removed vite and used create-react-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,70 @@
 import * as esbuid from 'esbuild-wasm'
-import React, { useState, useEffect, useRef } from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import {unpkgPathPlugin} from "./plugins/unpkg-path-plugin";
 import {fetchPlugin} from "./plugins/fetch-plugin";
 
 
 const App = () => {
     const ref = useRef<any>(null);
-    const  [input, setInput ] = useState('');
-    const [code, setCode] = useState('');
+    const iframeRef = useRef<any>(null);
+    const [input, setInput] = useState('');
 
     const startService = async () => {
-       ref.current = await esbuid.startService({
+        ref.current = await esbuid.startService({
             worker: true,
             wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
         })
 
     }
-    useEffect(() =>{
+    useEffect(() => {
         startService();
-    },[])
-    const onClick= async () => {
-        if(!ref.current){
+    }, [])
+    const onClick = async () => {
+        if (!ref.current) {
             return;
         }
+        iframeRef.current.srcdoc = html;
+
         const result = await ref.current.build({
             entryPoints: ['index.js'],
             bundle: true,
             write: false,
             plugins: [unpkgPathPlugin(), fetchPlugin(input)],
             define: {
-                global : 'window',
-    // "process.env.NODE_ENV": '"production"',
+                global: 'window',
+                "process.env.NODE_ENV": JSON.stringify('production'),
             },
         })
-
-        console.log("result",result)
-        setCode(result.outputFiles[0].text)
+        iframeRef.current.contentWindow.postMessage(result.outputFiles[0].text, '*')
     }
-    return(
+
+    const html = `
+            <html>
+            <head></head>
+            <body>
+            <div id="root"></div>
+            <script>
+                window.addEventListener('message',(event)=>{
+                    try{
+                       eval(event.data) 
+                    }catch(err){
+                        const root = document.querySelector('#root');
+                        root.innerHTML='<div style="color: red;"><h4>Runtime Error:</h4>'+ err +'</div>'
+                        console.error(err);
+                    }
+                    
+                }, false)
+                </script>
+            </body>
+            </html>
+    `
+    return (
         <div>
             <textarea value={input} onChange={e => setInput(e.target.value)}/>
-                <div>
-                    <button onClick={onClick}>Submit</button>
-                </div>
-                <pre>{code}</pre>
+            <div>
+                <button onClick={onClick}>Submit</button>
+            </div>
+            <iframe title="code_preview" ref={iframeRef} sandbox="allow-scripts" srcDoc={html}/>
         </div>
     )
 }
